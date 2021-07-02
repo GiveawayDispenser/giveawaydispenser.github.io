1ce02e8b865508762a8d433ddf1369591058c11a
GiveawayDispenser <86185576+GiveawayDispenser@users.noreply.github.com>
2021-07-02 02:31:10 -0700

    Add files via upload

---
+++ scymplex/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    let $Table = $("#Table > tbody");
+    $.getJSON("rolls.json", function(data) {
+        $.each(data, function(index, value) {
+            var tRow = "<tr><td>" + value.name + "</td><td>" + value.roll + "</td><td>" + value.pity + "</td></tr>";
+            $Table.append(tRow);
+        });
+        $('#Table').DataTable({
+            order: [1,'desc'].
+            pageLength: 50,
+            lengthMenu: [
+                [8, 10, 20, -1],
+                [8, 10, 20, 100]
+            ]
+
+        });
+    });
+
+});
